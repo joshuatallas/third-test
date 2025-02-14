95e84a1d8295e61c26f06a567d5a80fd41d5463c
joshuatallas <tallas.joshua@gmail.com>
2025-02-14 13:37:54 -0700

    change file

--- longest_word.py
+++ longest_word.py
@@ -1,10 +1,12 @@
 
 
-def top_5_longest_words_from_file(words_new.txt):
+# def top_5_longest_words_from_file(words_new.txt):
 
-    with open('words_new.txt', 'r') as f:
-        content = file.read()
-        words = content.split()
+#     with open('words_new.txt', 'r') as f:
+#         content = file.read()
+#         words = content.split()
 
-    top_words = sorted(words, key=len, reverse=True)[:5]
-return top_words
+#     top_words = sorted(words, key=len, reverse=True)[:5]
+# return top_words
+
+print("Hello, World!")
